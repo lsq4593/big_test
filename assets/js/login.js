3fe9964fbc1001bea395e7f065408e90dc124e77
LSQ <158170685@qq.com>
2021-01-08 10:54:38 +0800

    完成了登录模块

---
+++ assets/js/login.js
@@ -0,0 +1,80 @@
+$(function () {
+    $('.reg-box').hide()
+    // 点击注册账号链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    // 点击去登录链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+
+    // 从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    // 通过form.verify() 函数自定义校验规则
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须是6-12位，且不能出现空格']
+        ,
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败，则return一个提示显示即可
+            var pwd = $('.reg-box[name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1.阻止默认的提交行为
+        e.preventDefault()
+        // 2.发起Ajax请求
+        var data = {
+            username: $('#form_reg[name=username]').val(),
+            password: $('#form_reg[name=password]').val()
+        }
+        $.POST('http://api-breakingnews-web.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+
+    // 监听登录表单提交事件
+    $('#form_login').on('submit', function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        // 发起AJAX请求
+        $.ajax({
+            url: 'http://api-breakingnews-web.itheima.net/api/login',
+            type: 'POST',
+            // 快速获取表单数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败！')
+                }
+                layer.msg('登陆成功！')
+                // 将数据保存到本地
+                localStorage.setItem('token', res.token)
+                console.log(res.token);
+                // 跳转到主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
+})
